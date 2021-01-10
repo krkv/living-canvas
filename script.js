c7c841ccd709919c39176601cf943cb4ec622371
Rodion Kryuchkov <rodion.krjutskov@gmail.com>
2021-01-10 16:58:04 +0200

    add expanded block logic

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const blocks = [];
+const MAX_AGE = 10000;
+const MATING_COOLDOWN = 5000;
+const MAX_BLOCKS = 100;
+
+let blocks = [];
 let canvasWidth = null;
 let canvasHeight = null;
 let ctx = null;
@@ -13,8 +17,16 @@ function randomDirection() {
 }
 
 class Block {
-	constructor() {
-		this.s = Math.floor(Math.random() * 10) + 5
+	constructor(id) {
+		this.id = id
+
+		this.s = 25 //Math.floor(Math.random() * 50) + 10
+
+		this.age = 0
+		this.vitality = 0.9 + Math.random() / 10
+		this.alive = true
+		this.gender = Math.floor(Math.random() * 2)
+		this.fertile = true
 
 		this.x = Math.floor(Math.random() * (canvasWidth - this.s))
 		this.y = Math.floor(Math.random() * (canvasHeight - this.s))
@@ -24,11 +36,16 @@ class Block {
 			"y": randomDirection()
 		}
 		
-		this.r = Math.floor(Math.random() * 155) + 100
-		this.g = Math.floor(Math.random() * 155) + 100
-		this.b = Math.floor(Math.random() * 155) + 100
+		this.r = Math.floor(Math.random() * 255)
+		this.g = Math.floor(Math.random() * 255)
+		this.b = Math.floor(Math.random() * 255)
 	}
 };
+
+function areOverlapping(block1, block2) {
+	return (block2.x > block1.x - block2.s + 1) && (block2.x < block1.x + block2.s - 1) &&
+		(block2.y > block1.y - block2.s + 1) && (block2.y < block1.y + block2.s - 1)
+}
 
 function drawBlock(block) {
 	ctx.fillStyle = `rgb(${block.r},${block.g},${block.b})`;
@@ -44,7 +61,7 @@ function changeDirection(block) {
 
 function moveBlock(block) {
 	const roll = Math.random();
-	if (roll < 0.40) {
+	if (roll < 0.01) {
 		changeDirection(block);
 	};
 	if (block.x + block.direction.x + block.s > canvasWidth || block.x + block.direction.x < 0) {
@@ -57,14 +74,79 @@ function moveBlock(block) {
 	block.y = block.y + block.direction.y;
 };
 
+function checkOverlaps(block) {
+	blocks.forEach(b => {
+		if (b.id != block.id && areOverlapping(b, block)) {
+			makeOffspring(block, b)
+		}
+	})
+}
+
+function makeOffspring(block1, block2) {
+	if (block1.gender != block2.gender && block1.fertile && block2.fertile && blocks.length < MAX_BLOCKS) {
+		const roll = Math.random();
+		if (roll > 0.5) {
+			block = new Block(blocks.length)
+			block.x = block1.x
+			block.y = block2.y
+
+			const rollC = Math.random();
+			if (rollC < 0.5) {
+				block.r = block1.r
+				block.g = block1.g
+				block.b = block2.b
+			} else {
+				block.r = block2.r
+				block.g = block2.g
+				block.b = block1.b
+			}
+
+			block1.fertile = false
+			block2.fertile = false
+
+			window.setTimeout(restoreFertility, MATING_COOLDOWN, block1)
+			window.setTimeout(restoreFertility, MATING_COOLDOWN, block2)
+
+			blocks.push(block);
+			console.log("A block was born. Population:", blocks.length)
+		};
+	}
+}
+
+function restoreFertility(block) {
+	block.fertile = true;
+}
+
+function killBlock(block) {
+	if (block.age * block.vitality > MAX_AGE) {
+		roll = Math.random();
+		if (roll < 0.50) {
+			block.alive = false
+			console.log("A block has died. Pupulation:", blocks.length)
+		}
+	}
+}
+
+function removeDeadBlocks() {
+	blocks = blocks.filter(block => block.alive)
+}
+
+function ageBlock(block) {
+	block.age = block.age + 1
+}
+
 function tick() {
 	blocks.forEach(drawBlock)
+	blocks.forEach(checkOverlaps)
+	blocks.forEach(ageBlock)
+	blocks.forEach(killBlock)
+	removeDeadBlocks()
 	blocks.forEach(moveBlock)
 };
 
 function start() {
-	for (let index = 0; index < 1000; index++) {
-		block = new Block()
+	for (let index = 0; index < 25; index++) {
+		block = new Block(index)
 		blocks.push(block)
 	}
 };
